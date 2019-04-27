83e7311f408abf41120e013805ed2d487fb33b6e
MrMelon54 <48031324+MrMelon54@users.noreply.github.com>
2019-04-28 00:10:04 +0100

    More logging

--- build.py
+++ build.py
@@ -1,25 +1,27 @@
 import glob, requests, json
 g=glob.glob("modules/*.js")
 o="var ktaneModuleCommandInterface={"
-print("Building Modules:")
+log=""
+log+="Building Modules:"
 for item in g:
     f=open(item,'r')
     c=f.read()
     if not "//**no add**" in c:
         theitem=json.loads(c.split('$$')[1])
         theitem['script']="%%$$%%"
-        print("  - "+theitem['name'])
+        log+="\n  - "+theitem['name']
         o+='"'+item.replace('modules\\','').replace('modules/','').replace('.js','')+'":'
         o+=json.dumps(theitem).replace('"%%$$%%"','(bombinfo,args)=>{'+c+'}')
         o+=','
     f.close()
 o+='thisIsTheLastItemAndItIsNotActuallyNeededButIPutItHereAsAJoke:{"name":"","alias":[],"script":()=>{}}};'
 url = 'https://javascript-minifier.com/raw'
-data = {'input':o}
+data = {'input':o+"\n/* Log\n"+log+"\n*/"}
 response = requests.post(url, data=data).text
 f=open('ktaneModules.js','w')
 f.write(response)
 f.close()
+print(log)
 if(response[:3]!="var"):
     print(response)
     input("Build error press enter to quit")
