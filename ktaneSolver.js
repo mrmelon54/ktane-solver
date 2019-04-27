24504261b25e252e8a6ac25869e61cbf3a210570
MrMelon <melonmonster.54@gmail.com>
2019-04-27 11:27:43 +0100

    Started adding module loader

--- ktaneSolver.js
+++ ktaneSolver.js
@@ -1,6 +1,9 @@
 function KtaneSolver() {
     this.moduleList=theModuleList;
     this.init=()=>{
+        for(var i=0;i<this.moduleList.modules.length;i++) {
+            $.getScript(this.moduleList.modules[i].script);
+        }
     }
     this.callback=()=>{};
 }
