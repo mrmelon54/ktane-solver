85a5a76f96f69a0762a05f990fecf319289081f3
MrMelon54 <48031324+MrMelon54@users.noreply.github.com>
2019-04-28 22:19:59 +0100

    Added word to number

--- ktaneSolver.js
+++ ktaneSolver.js
@@ -137,6 +137,9 @@ function KtaneSolver() {
     this.toLetter = (t) => {
         return ktaneNatoToLetter(t);
     }
+    this.toNumber = (t) => {
+        return ktaneWordToNumber(t);
+    }
 }
 
 function ktaneNatoToLetter(t) {
@@ -176,6 +179,17 @@ function ktaneLetterToNato(t) {
         z: "zulu"
     };
     return convertor[t.toLowerCase()];
+}
+
+function ktaneWordToNumber(t) {
+    var convertor={one:1,two:2,three:3,four:4,five:5,six:6,seven:7,eight:8,nine:9,ten:10,eleven:11,twelve:12,thirteen:13,fourteen:14,
+                   fifteen:15,sixteen:16,seventeen:17,eighteen:18,nineteen:19,twenty:20,thirty:30};
+    try {
+        return convertor[t].toString();
+    } catch(e) {
+        if(isNAN(parseInt(t)))return null;
+        return t;
+    }
 }
 
 function KtaneBombInfo() {
